c3d583e1bc087e40ad366bfe721c2ed697d09c8d
Salvatore Rappoccio <rappoccio@gmail.com>
2019-10-10 16:39:37 -0400

    Implementing Jim's suggestion

--- coord_transform.py
+++ coord_transform.py
@@ -1,11 +1,21 @@
 import numpy as np
 
+def geometric_cross(A,B):
+    result = A.empty_like()
+    result["fX"], result["fY"], result["fZ"] = A._cross(B)
+    return result
+
+def array_rotate_axis(A, u, alpha):
+    result = A.empty_like()
+    result["fX"], result["fY"], result["fZ"] = A.rotate_axis(u, alpha)
+    result["fE"] = A["fE"]
+    return result
 
 def coord_rotation(a, toaxis):
     '''
     Get the axis and angle of rotation for a coordinate transformation
     where vector "a" is rotated to be along axis "toaxis". 
     '''
-    alpha = np.arccos( a.dot(toaxis)/( abs(a) * abs(toaxis) ))
-    axis_perp = a.cross(toaxis).unit
+    alpha = a.angle(toaxis)
+    axis_perp = geometric_cross(a, toaxis).unit
     return axis_perp, alpha
